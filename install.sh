b7b61c58c4be091841bd5725ff6ec4817b436677
rk1 <rk1@example.com>
2013-11-06 21:45:33 +0200

    migrating to vundle - cleanup

--- install.sh
+++ install.sh
@@ -50,14 +50,3 @@ for z in ${H[@]}; do
     ln -s ${P}/${z} ./${z}
 done
 popd >/dev/null
-
-# git submodules
-
-echo "Git syncing"
-git submodule sync
-git submodule init
-git submodule update
-git submodule foreach git pull origin master
-git submodule foreach git submodule init
-git submodule foreach git submodule update
-echo "Done!"
