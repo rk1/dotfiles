60001aae3d2a94678fe7f6dca2adfe6794dcc2c7
rk1 <rk1@example.com>
2024-11-26 08:00:59 +0200

    Telescope instead of fzf

--- nvim/.config/nvim/lua/plugins/fzf-checkout.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'stsewd/fzf-checkout.vim',
-  config = function()
-    vim.g.fzf_checkout_git_options = '--sort=-committerdate'
-    vim.keymap.set('n', '<Leader>gb', ':GBranches --locals<CR>', { noremap = true, silent = true })
-  end
-}
