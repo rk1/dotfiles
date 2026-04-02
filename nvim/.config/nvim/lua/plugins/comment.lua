a3aa40af257d90c564e723a0c0622807ecf965a2
rk1 <rk1@example.com>
2026-04-02 18:38:01 +0300

    Fix comment.lua

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -4,7 +4,9 @@ return {
 		"JoosepAlviste/nvim-ts-context-commentstring",
 	},
 	config = function()
-		require("ts_context_commentstring").setup({})
+		require("ts_context_commentstring").setup({
+			enable_autocmd = false,
+		})
 		require("Comment").setup({
 			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 		})
