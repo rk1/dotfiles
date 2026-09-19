12e7ea448ded5f96590315e0081e535e2b7d6dd2
rk1 <rk1@example.com>
2026-09-19 17:42:14 +0300

    Treesitter updates

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,21 +1,47 @@
+local parsers = {
+	"css",
+	"html",
+	"javascript",
+	"json",
+	"lua",
+	"tsx",
+	"typescript",
+	"vim",
+}
+
+local filetypes = {
+	"css",
+	"html",
+	"javascript",
+	"javascriptreact",
+	"json",
+	"lua",
+	"typescript",
+	"typescriptreact",
+	"vim",
+}
+
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	-- The main branch does not support lazy-loading.
+	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"lua",
-				"vim",
-				"javascript",
-				"typescript",
-				"tsx",
-				"json",
-				"html",
-				"css",
-			},
-			highlight = {
-				enable = true,
-			},
+		require("nvim-treesitter").install(parsers)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("TreesitterStart", { clear = true }),
+			pattern = filetypes,
+			desc = "Enable treesitter highlighting and folding",
+			callback = function()
+				if not pcall(vim.treesitter.start) then
+					return
+				end
+
+				vim.wo[0][0].foldmethod = "expr"
+				vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+			end,
 		})
 	end,
 }
