bd7946a7f89bf72b76973bed8799b250b6133765
rk1 <rk1@example.com>
2026-09-23 07:20:05 +0300

    Mason lsp updates

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -40,7 +40,7 @@ end
 return {
 	-- Mason: LSP server installer
 	{
-		"williamboman/mason.nvim",
+		"mason-org/mason.nvim",
 		cmd = "Mason",
 		opts = {
 			ui = {
@@ -55,9 +55,9 @@ return {
 	},
 	-- Mason-LSPConfig bridge
 	{
-		"williamboman/mason-lspconfig.nvim",
+		"mason-org/mason-lspconfig.nvim",
 		dependencies = {
-			"williamboman/mason.nvim",
+			"mason-org/mason.nvim",
 			"neovim/nvim-lspconfig",
 		},
 		lazy = false,
@@ -85,17 +85,24 @@ return {
 		config = function()
 			vim.diagnostic.config({
 				virtual_text = true,
-				signs = true,
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = "✘",
+						[vim.diagnostic.severity.WARN] = "▲",
+						[vim.diagnostic.severity.HINT] = "⚑",
+						[vim.diagnostic.severity.INFO] = "»",
+					},
+					numhl = {
+						[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+						[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+						[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+						[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+					},
+				},
 				underline = true,
 				update_in_insert = false,
 				severity_sort = true,
 			})
-
-			local signs = { Error = "✘", Warn = "▲", Hint = "⚑", Info = "»" }
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-			end
 
 			-- Attach keymaps when LSP attaches to a buffer
 			vim.api.nvim_create_autocmd("LspAttach", {
